9d3900a28cbf8abca857b814baaeedf7bd5a1115
Tatan1789 <123003140+Tatan1789@users.noreply.github.com>
2026-03-23 08:27:08 -0500

    feat: add data loading step to save output as CSV

--- src/load.py
+++ src/load.py
@@ -1,2 +1,15 @@
-def load_data():
-    print("Loading data...")
+import os
+
+def load_data(df):
+    if df is None:
+        print("No data to load")
+        return
+
+    output_path = "data/crypto_data.csv"
+
+    # crear carpeta si no existe
+    os.makedirs("data", exist_ok=True)
+
+    df.to_csv(output_path, index=False)
+
+    print(f"Data saved to {output_path}")
